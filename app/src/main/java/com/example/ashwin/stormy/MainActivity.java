733318880e9e5ee68c7227904146eeffa435d104
Ashwin Kumar <kashwin95kumar@gmail.com>
2016-07-04 21:12:29 +0530

    location services using the google play services api added to the app

--- app/src/main/java/com/example/ashwin/stormy/MainActivity.java
+++ app/src/main/java/com/example/ashwin/stormy/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.ashwin.stormy;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.GpsStatus;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -20,10 +30,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.jar.Manifest;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,12 +52,26 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
-    public static  final  String TAG = MainActivity.class.getSimpleName();
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    public Location location = null;
+
+    private LocationRequest mLocationRequest;
+
+    private String addr ;
+
+    //private double latitude;
+    //private double longitude;
 
     private CurrentWeather mCurrentWeather;
-    
+    private GoogleApiClient mGoogleApiClient;
 
     @BindView(R.id.humidityValue) TextView mHumidityValue;
     @BindView(R.id.precipitationValue) TextView mPrecipitationValue;
@@ -56,28 +89,51 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = LocationRequest.create()
+                .setInterval(10*1000)
+                .setFastestInterval(1*1000)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
         mProgressBar.setVisibility(View.INVISIBLE);
 
-        final double latitude = 37.8267;
-        final double longitude = -122.423;
+        //final double latitude = location.getLatitude();
+        //final double longitude = location.getLongitude();
 
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getForecast(latitude,longitude);
+                getForecast(location.getLatitude(), location.getLongitude());
             }
         });
 
 
+       // getForecast(latitude, longitude);
 
-        getForecast(latitude,longitude);
+
 
     }
 
-    private void getForecast(double latitude,double longitude) {
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected())
+            mGoogleApiClient.disconnect();
+    }
+
+    private void getForecast(double latitude, double longitude) {
         String apiKey = "d0e1dd21273dac6ef385281a041822ae";
-        String forecastUrl = "https://api.forecast.io/forecast/"+ apiKey +"/" + latitude + "," + longitude ;
-        if(isNetworkAvaialable()) {
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+        if (isNetworkAvaialable()) {
             mRefreshImageView.setVisibility(View.INVISIBLE);
             mProgressBar.setVisibility(View.VISIBLE);
 
@@ -106,7 +162,7 @@ public class MainActivity extends ActionBarActivity {
                     try {
                         String jsonData = response.body().string();
                         if (response.isSuccessful()) {
-                            Log.v(TAG, jsonData);
+                           // Log.v(TAG, jsonData);
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
@@ -122,21 +178,19 @@ public class MainActivity extends ActionBarActivity {
                                 }
                             });
                         } else {
-                            Log.v(TAG, jsonData);
+                           // Log.v(TAG, jsonData);
                             alertUserAboutError();
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "Exception caught", e);
-                    }
-                    catch (JSONException e){
-                        Log.e(TAG,"EXCEPTION CAUGHT",e);
+                    } catch (JSONException e) {
+                        Log.e(TAG, "EXCEPTION CAUGHT", e);
                     }
 
                 }
             });
-        }
-        else{
-            Toast.makeText(MainActivity.this,"Some Problem with the Network",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Some Problem with the Network", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -148,6 +202,8 @@ public class MainActivity extends ActionBarActivity {
         mSummary.setText(mCurrentWeather.getSummary());
         Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
         mIconView.setImageDrawable(drawable);
+        mLocationLabel.setText(addr);
+
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
@@ -168,10 +224,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private boolean isNetworkAvaialable() {
-        ConnectivityManager manager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();
         boolean isAvailable = false;
-        if(networkInfo != null && networkInfo.isConnected()){
+        if (networkInfo != null && networkInfo.isConnected()) {
             isAvailable = true;
         }
         return isAvailable;
@@ -179,8 +235,70 @@ public class MainActivity extends ActionBarActivity {
 
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
-        dialog.show(getFragmentManager(),"error_dialog");
+        dialog.show(getFragmentManager(), "error_dialog");
 
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "Location Services connected.");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location==null){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
+        }
+        else{
+            Geocoder gcd = new Geocoder(MainActivity.this, Locale.getDefault());
+            List<Address> addresses = null;
+            try {
+                addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (addresses != null && addresses.size() > 0)
+                addr = addresses.get(0).getLocality();
+            getForecast(location.getLatitude(),location.getLongitude());
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+       // Log.d(TAG,location.toString());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+       // Log.i(TAG,"Location Services suspended. Please reconnect");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        if(connectionResult.hasResolution()){
+            try{
+                connectionResult.startResolutionForResult(this,CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            }
+            catch(IntentSender.SendIntentException e){
+                e.printStackTrace();
+            }
+        }
+        else {
+          //  Log.i(TAG,"Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
 }
